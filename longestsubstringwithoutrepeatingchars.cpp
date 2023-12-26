b2f4765e4845dc0ba87ea7656f1b5b64950317d1
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2023-12-26 13:39:45 +0530

    Modified

--- longestsubstringwithoutrepeatingchars.cpp
+++ longestsubstringwithoutrepeatingchars.cpp
@@ -50,3 +50,31 @@ using namespace std;
 
 //Watch rohit negi's video for this
 //Link: https://www.youtube.com/watch?v=swBjx46TSP4
+
+
+//Using unordered map
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int maxLength = 0;
+        unordered_map<char, int> charMap;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (charMap.count(s[right]) == 0 || charMap[s[right]] < left) {
+                charMap[s[right]] = right;
+                maxLength = max(maxLength, right - left + 1);
+            } else {
+                left = charMap[s[right]] + 1;
+                charMap[s[right]] = right;
+            }
+        }
+        
+        return maxLength;
+}
+
+//T.C = O(N)
+//S.C = O(N)
