d83090f0e00e585e1da13c66eadacd42e7c43af6
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2023-12-30 00:03:27 +0530

    Leetcode

---
+++ findwordscontainingchar.cpp
@@ -0,0 +1,62 @@
+/*
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["leet","code"], x = "e"
+Output: [0,1]
+Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+Example 2:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+Output: [0,2]
+Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+Example 3:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+Output: []
+Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+ 
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+//My solution:Using two nested loops
+//T.C = O(N*M)
+/*
+The space complexity is O(n), where n is the number of words in the vector. 
+This is because we are using a vector ans to store the indices of words that contain 
+the character x. The size of this vector is directly proportional to the number of words 
+that meet the condition.
+*/
+
+//S.C = O(N)
+
+  vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+
+        vector<string>::iterator it;
+
+        for(it=words.begin(); it!=words.end(); ++it){
+            string s = *it;
+
+             bool found = false;
+            for(int i = 0; i<s.length();i++){
+                if(s[i]== x){
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+              ans.push_back(distance(words.begin(), it));
+           }
+        }
+        return ans;
+}
