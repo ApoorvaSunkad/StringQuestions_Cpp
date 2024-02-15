93f5cdf5d7fc0fae60f3f5eaf074ca03ac97aedb
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-02-15 23:23:58 +0530

    GFG

---
+++ reverseaStringrecursively.cpp
@@ -0,0 +1,37 @@
+/*
+You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+Your Task:
+
+You only need to complete the function reverseWord() that takes s as parameter 
+and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000
+
+
+
+*/
+
+string reverseWord(string str){
+    int n = str.length();
+
+    if(n==0 || n==1){
+        return str;
+    }
+
+    return reverseWord(str.substr(1))+str[0];
+}
